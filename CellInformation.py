42eee7e85f70abf43085793c71ea0dcb0c8d2a88
Adam Mehdi <adammehdi98765@gmail.com>
2022-12-17 15:21:15 -0500

    Added history checking for the map

--- CellInformation.py
+++ CellInformation.py
@@ -1,14 +1,22 @@
-from pygame import *
+
 from math import *
+from datetime import datetime
+from time import time
 
 class Map:
     def __init__(self, width: int, height: int, startPreys: int, startPreds: int, maxPreys: int, maxPreds: int):
+        '''Creates a map object to hold cells'''
         self.width = width
         self.height = height
         self.startPreys = startPreys
         self.startPreds = startPreds
         self.maxPreys = maxPreys
         self.maxPreds = maxPreds
+
+        self.filename = datetime.now().strftime("Histories/%Y%m%d%H%M%S.txt")
+        self.timer = time()
+
+        self.file = open(self.filename, "w")
 
         self.preyList = []
         for i in range(self.startPreys):
@@ -18,9 +26,24 @@ class Map:
         for i in range(self.startPreds):
             self.predList.append("pred")
 
+    def updateHistory(self):
+        if (self.timer - time()) > 1000:
+            self.timer = time()
+            self.file.write("test")
+            #write history into file
+
+    def kill(self):
+        '''Signals program ending and ensures accompanying files are also closed'''
+        self.file.close()
+
     def update(self):
+        '''Updates all cells in the map for the current frame'''
         pass
 
     def draw(self, screen):
+        '''Draws all cells in the map for the current frame'''
         pass
 
+m = Map(1, 1, 1, 1, 1, 1)
+while True:
+    m.updateHistory()
