81caa4a79e1b5e04e64b160a1dd08ff280f4d2bc
Henning Jiang <henningjiang888@gmail.com>
2022-12-17 15:26:04 -0500

    adding cell superclass

--- CellInformation.py
+++ CellInformation.py
@@ -1,4 +1,3 @@
-
 from math import *
 from datetime import datetime
 from time import time
@@ -43,6 +42,52 @@ class Map:
     def draw(self, screen):
         '''Draws all cells in the map for the current frame'''
         pass
+
+class Cell:
+    MAXIMUM_SPEED = 100
+    MAXIMUM_TURN_SPEED = 0.1
+    DEFAULT_ANGLE = 0
+    EMPTY_NETWORK = 0
+    CELL_RADIUS = 40
+    PREY_COLOUR = (0,255,0)
+    PREDATOR_COLOUR = (255,0,0)
+
+    def __init__(self, startingNetwork = EMPTY_NETWORK, previousGenerationNumber = -1, xyPos):
+        self.speed = 0
+        self.angle = DEFAULT_ANGLE
+        self.angularVelocity = 0
+        self.collisionModifier = [0,0]
+        self.generationNumber = previousGenerationNumber + 1
+
+    """ Modifies Cell angle by Cell angularVelocity """
+    def turn():
+        angle += angularVelocity
+        angle %= 2 * pi
+
+    """ Modifies position according to speed, angle and collisionModifier """
+    def move():
+        xyPos[0] += speed * cos(angle)
+        xyPos[1] += speed * sin(angle)
+    
+    """ Detect if collision is happening and modify collisionModifier.
+
+    We check how close this cell and otherCell are to each other. If they are colliding,
+    apply a movement on both cells directly away from each other that will be processed
+    in `move()`.
+    
+    """
+    def findCollision(otherCell):
+        distance = (xyPos[0] - otherCell.xyPos[0])**2 + (xyPos[1] - otherCell.xypos[1])**2
+
+        if distance > (CELL_RADIUS * 2)**2:
+            return
+        
+        raise NotImplementedError()
+
+    
+    """ Draw the cell on `canvas` """
+    def draw(canvas):
+        raise NotImplementedError()
 
 m = Map(1, 1, 1, 1, 1, 1)
 while True:
